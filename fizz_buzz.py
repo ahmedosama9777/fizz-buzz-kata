6d65fdd9ed808923aae6313cb3559f330d4333fe
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-14 14:37:28 +0200

    Add test greater than range

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,15 +1,20 @@
-class FizzBuzz():
+class FizzBuzz:
     def run(self, number: int) -> str:
-        
+        self._validate_input(number)
+
         result: str = ""
 
-        if number < 1:
-            raise ValueError
         if number % 3 == 0:
             result += "Fizz"
         if number % 5 == 0:
             result += "Buzz"
         if result == "":
             result += str(number)
-        
+
         return result
+
+    def _validate_input(self, number: int):
+        if number < 1:
+            raise ValueError("Number must be greater than 0")
+        elif number > 100:
+            raise ValueError("Number must be less than 100")
--- fizz_buzz_test.py
+++ fizz_buzz_test.py
@@ -1,22 +1,30 @@
 from unittest import TestCase
+
 from fizz_buzz import FizzBuzz
+
 
 class TestFizzBuzz(TestCase):
     def setUp(self) -> None:
         self.fizz_buzz = FizzBuzz()
-    
+
     def test_multiples_of_three(self):
         self.assertEqual(self.fizz_buzz.run(3), "Fizz")
-    
+
     def test_multiples_of_five(self):
         self.assertEqual(self.fizz_buzz.run(5), "Buzz")
-    
+
     def test_multiple_of_five_and_three(self):
         self.assertEqual(self.fizz_buzz.run(45), "FizzBuzz")
-    
+
     def test_neither_multiple_of_three_nor_five(self):
         self.assertEqual(self.fizz_buzz.run(1), "1")
-    
+
     def test_input_less_than_range(self):
-        with self.assertRaises(ValueError):
+        with self.assertRaises(ValueError) as e:
             self.fizz_buzz.run(0)
+        self.assertEqual(str(e.exception), "Number must be greater than 0")
+
+    def test_input_greater_than_range(self):
+        with self.assertRaises(ValueError) as e:
+            self.fizz_buzz.run(101)
+        self.assertEqual(str(e.exception), "Number must be less than 100")
