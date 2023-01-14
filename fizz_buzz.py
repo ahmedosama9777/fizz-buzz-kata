6fb8c61333e35165a6b361cc253aa21563f7af66
ahmedosama9777 <ahmedosama9777@gmail.com>
2023-01-14 14:05:50 +0200

    Add test of creating FizzBuzz instance

---
+++ fizz_buzz.py
@@ -0,0 +1,3 @@
+class FizzBuzz():
+    def run(self):
+        return "0"
--- fizz_buzz_test.py
+++ fizz_buzz_test.py
@@ -1,4 +1,8 @@
 from unittest import TestCase
+from fizz_buzz import FizzBuzz
 
 class TestFizzBuzz(TestCase):
+    def test_create_fizz_buzz(self):
+        fizz_buzz = FizzBuzz()
+        self.assertEqual(fizz_buzz.run(), "0")
     
